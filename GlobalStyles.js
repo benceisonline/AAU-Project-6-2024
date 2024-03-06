ff87d523359533ff9d186789456139f6181d36c0
Louise Steffens <62352035+loui981c@users.noreply.github.com>
2024-03-06 09:42:27 +0100

    folder struc & start of styles (#1)

---
+++ GlobalStyles.js
@@ -0,0 +1,48 @@
+import React from 'react'; 
+import { StyleSheet } from 'react-native';
+
+export const colors = StyleSheet.create({
+  plus: {
+    backgroundColor: '#1C4EFF',
+  },
+  headerInactive: {
+    backgroundColor: '#7A7373',
+  },
+  headerBackground: {
+    backgroundColor: '#FCFCFC',
+  },
+  topic: {
+    backgroundColor: '#158A19',
+    fontFamily: 'Work Sans',
+    fontSize: '8px',
+    fontWeight: 400
+  },
+  newsBackground: {
+    backgroundColor: '#FEFEFE',
+  },
+  breakingNews: {
+    backgroundColor: '#FAFF00',
+    fontFamily: 'Karla',
+    fontSize: '20px',
+    fontStyle: italic,
+    fontWeight: 700,
+  },
+  timeStamp: {
+    backgroundColor: '#7B7676',
+    fontFamily: 'Work Sans',
+    fontSize: '10px',
+    fontWeight: 400
+  },
+  journalistName: {
+    backgroundColor: '#020202',
+    fontFamily: 'Work Sans',
+    fontSize: '10px',
+    fontWeight: 500,
+  },
+  headerTitle: {
+    backgroundColor: '#111010',
+    fontFamily: 'Karla',
+    fontSize: '14px',
+    fontWeight: 500,
+  }
+});
