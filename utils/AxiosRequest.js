5e07823985b6b29189a022caafd29eaa912efa07
Louise Steffens <62352035+loui981c@users.noreply.github.com>
2024-04-15 10:58:41 +0200

    Database (#34)
    * supabase init and create userId
    
    * secret keys
    
    * Update app.json
    
    * Update app.json
    
    ---------
    
    Co-authored-by: Bence Szabo <benceszabo657@gmail.com>

--- utils/AxiosRequest.js
+++ utils/AxiosRequest.js
@@ -1,6 +1,6 @@
 const axios = require('axios').default;
 
-const localUrl = '172.20.10.2';
+const localUrl = '192.168.68.102';
 const apiUrl = `http://${localUrl}:8000`;
 
 export const fetchPredictions = async (userId, noOfRecommendations) => {
