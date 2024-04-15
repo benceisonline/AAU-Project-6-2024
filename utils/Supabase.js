7926ab12ee383e3318c90108cf0a1b777cbc4353
Louise Steffens <foldoysteffenslouise@gmail.com>
2024-04-15 10:12:36 +0200

    secret keys

--- utils/Supabase.js
+++ utils/Supabase.js
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
+import keys from '../env.js'
 
-export const supabase = createClient('https://rloyhaxoudsrlnodgmjk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsb3loYXhvdWRzcmxub2RnbWprIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI3NzQ2NzYsImV4cCI6MjAyODM1MDY3Nn0.3inRGcRHWUbTFu1bij7cjlKTPSrdSlmP27eetHXIi_k')
+export const supabase = createClient(keys.SUPABASE_URL, keys.SUPABASE_ANON_KEY);
