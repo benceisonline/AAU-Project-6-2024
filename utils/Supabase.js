93f0fa565307f04c10974c06bff1227c3c3e30d9
Louise Steffens <foldoysteffenslouise@gmail.com>
2024-05-20 00:53:06 +0200

    refactor and tests

--- utils/Supabase.js
+++ utils/Supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import keys from '../env.js'
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env'
 
-export const supabase = createClient(keys.SUPABASE_URL, keys.SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
