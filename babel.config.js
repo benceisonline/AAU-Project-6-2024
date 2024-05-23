f5c8908b1eb706837f7868266181abe6669ca17c
Anders Mazen Youssef <andersmazen@gmail.com>
2024-05-23 18:20:06 +0200

    hotfix

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,10 @@ module.exports = function(api) {
 	api.cache(true);
 	return {
 		presets: ['babel-preset-expo'],
-    // plugins: [
-    //   [
-    //     'module:react-native-dotenv',
-    //     {
-    //       moduleName: '@env',
-    //       path: '.env',
-    //     },
-    //   ],
-    // ],
+    "plugins": [
+      ["@babel/plugin-transform-runtime", {
+        "regenerator": true
+      }]
+    ]
 	};
 };
